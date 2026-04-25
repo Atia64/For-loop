02c888062bb0608b83011f5f63e81fb0a9bb3eda
Atia Akter <atiaakter6666@gmail.com>
2026-04-25 18:33:00 +0600

    first commit

---
+++ peramid 9.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter a number:";
+cin>>n;
+int i,j,k;
+for(i=1;i<=n;i++){
+    for(j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(k=1;k<=2*i-1;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+for(i=n-1;i>=1;i--){
+    for(j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(k=1;k<=2*i-1;k++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+return 0;
+}
