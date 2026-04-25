5e6fc8a3e26b054f0e542e640cecb74d0a89c7bd
Atia Akter <atiaakter6666@gmail.com>
2026-04-25 18:33:28 +0600

    first commit

---
+++ peramid 10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n=3;
+int i,j,k;
+for(i=1;i<=n;i++){
+    for(j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(k=1;k<=2*i-1;k++){
+        cout<<k;
+    }
+    cout<<endl;
+}
+for(i=n-1;i>=1;i--){
+    for(j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(k=1;k<=2*i-1;k++){
+        cout<<k;
+    }
+    cout<<endl;
+}
+return 0;
+}
